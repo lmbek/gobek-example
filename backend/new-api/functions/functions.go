ab6b936381c749fce2e78353b5fe0eda2a4e0324
Lars Morten Bek <lars@beksoft.dk>
2023-06-26 22:21:05 +0200

    functions.go: * Refactored NextLayer to CurrentLayer * Refactored CuttedEndpoint to ReducedEndpoint * Added ReducedFirstLayerEndpoint that should manage the first endpoint which is /api/ while the others will be endpoints such as user/{id}/name

--- backend/new-api/functions/functions.go
+++ backend/new-api/functions/functions.go
@@ -2,18 +2,29 @@ package functions
 
 import "strings"
 
-func NextLayer(endpoint string) string {
+func CurrentLayer(endpoint string) string {
+	// /api/personal//user/995/name
+
 	splitted := strings.Split(endpoint, "/")
-	if len(splitted) > 0 {
-		return splitted[0] // identifier reached, maybe?
+	// ["", "api", "user", "995", "name"]
+	if len(splitted) >= 1 {
+		return splitted[0] // /personal//user/995/name
 	}
 	return ""
 }
 
-func CuttedEndpoint(endpoint string) string {
+func ReducedEndpoint(endpoint string) string {
 	splitted := strings.Split(endpoint, "/")
-	if len(splitted) > 0 {
+	if len(splitted) >= 1 {
 		return strings.Join(splitted[1:], "/")
+	}
+	return ""
+}
+
+func ReducedFirstLayerEndpoint(endpoint string) string {
+	splitted := strings.Split(endpoint, "/")
+	if len(splitted) >= 2 {
+		return strings.Join(splitted[2:], "/")
 	}
 	return ""
 }
