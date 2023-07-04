639b54b4b6a05db52d84206184c3dab4ebf77cd2
Lars Morten Bek <lars@beksoft.dk>
2023-07-04 19:11:54 +0200

    api_test.go: Modified * Fix: Updated all tests. They now pass again

--- backend/tests/api_test.go
+++ backend/tests/api_test.go
@@ -4,6 +4,8 @@ import (
 	"api"
 	"errors"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -26,7 +28,7 @@ func TestAPIUserEndpoint(test *testing.T) {
 	test.Run("call1 /api/user", func(test *testing.T) {
 
 		expected := errors.New("need identifier: /api/user/{id}").Error()
-		_, err := api.HandleEndpoint("/api/user")
+		_, err := api.HandleEndpoint("/api/user", nil, nil)
 		result := err.Error()
 		//result := userData.(api.JSONDataResponse).Data
 		if result != expected {
@@ -37,7 +39,7 @@ func TestAPIUserEndpoint(test *testing.T) {
 	})
 	test.Run("call2 /api/user/", func(test *testing.T) {
 		expected := errors.New("need identifier: /api/user/{id}").Error()
-		_, err := api.HandleEndpoint("/api/user/")
+		_, err := api.HandleEndpoint("/api/user/", nil, nil)
 		result := err.Error()
 
 		if result != expected {
@@ -48,7 +50,7 @@ func TestAPIUserEndpoint(test *testing.T) {
 	})
 	test.Run("call3 /api/user/995", func(test *testing.T) {
 		expected := "995"
-		result, _ := api.HandleEndpoint("/api/user/995")
+		result, _ := api.HandleEndpoint("/api/user/995", nil, nil)
 
 		if result != expected {
 			test.Errorf("\t\tExpected %v, but got: %v \n", expected, result)
@@ -58,7 +60,7 @@ func TestAPIUserEndpoint(test *testing.T) {
 	})
 	test.Run("call4 /api/user/995/", func(test *testing.T) {
 		expected := "995"
-		result, _ := api.HandleEndpoint("/api/user/995/")
+		result, _ := api.HandleEndpoint("/api/user/995/", nil, nil)
 
 		if result != expected {
 			test.Errorf("\t\tExpected %v, but got: %v \n", expected, result)
@@ -68,7 +70,7 @@ func TestAPIUserEndpoint(test *testing.T) {
 	})
 	test.Run("call5 /api/user/996", func(test *testing.T) {
 		expected := errors.New("user id does not exist").Error()
-		_, err := api.HandleEndpoint("/api/user/996")
+		_, err := api.HandleEndpoint("/api/user/996", nil, nil)
 		result := err.Error()
 
 		if result != expected {
@@ -79,7 +81,7 @@ func TestAPIUserEndpoint(test *testing.T) {
 	})
 	test.Run("call6 /api/user/996/", func(test *testing.T) {
 		expected := errors.New("user id does not exist").Error()
-		_, err := api.HandleEndpoint("/api/user/996/")
+		_, err := api.HandleEndpoint("/api/user/996/", nil, nil)
 		result := err.Error()
 
 		if result != expected {
@@ -90,7 +92,7 @@ func TestAPIUserEndpoint(test *testing.T) {
 	})
 	test.Run("call7 /api/user/995/name", func(test *testing.T) {
 		expected := "LMB"
-		result, _ := api.HandleEndpoint("/api/user/995/name")
+		result, _ := api.HandleEndpoint("/api/user/995/name", nil, nil)
 
 		if result != expected {
 			test.Errorf("\t\tExpected %v, but got: %v \n", expected, result)
@@ -100,7 +102,7 @@ func TestAPIUserEndpoint(test *testing.T) {
 	})
 	test.Run("call8 /api/user/995/name/", func(test *testing.T) {
 		expected := "LMB"
-		result, _ := api.HandleEndpoint("/api/user/995/name/")
+		result, _ := api.HandleEndpoint("/api/user/995/name/", nil, nil)
 
 		if result != expected {
 			test.Errorf("\t\tExpected %v, but got: %v \n", expected, result)
@@ -110,7 +112,7 @@ func TestAPIUserEndpoint(test *testing.T) {
 	})
 	test.Run("call9 /api/user/996/name", func(test *testing.T) {
 		expected := errors.New("user id does not exist").Error()
-		_, err := api.HandleEndpoint("/api/user/996/name")
+		_, err := api.HandleEndpoint("/api/user/996/name", nil, nil)
 		result := err.Error()
 
 		if result != expected {
@@ -121,7 +123,7 @@ func TestAPIUserEndpoint(test *testing.T) {
 	})
 	test.Run("call10 /api/user/996/name/", func(test *testing.T) {
 		expected := errors.New("user id does not exist").Error()
-		_, err := api.HandleEndpoint("/api/user/996/name/")
+		_, err := api.HandleEndpoint("/api/user/996/name/", nil, nil)
 		result := err.Error()
 
 		if result != expected {
@@ -132,7 +134,7 @@ func TestAPIUserEndpoint(test *testing.T) {
 	})
 	test.Run("call11 /api/user/995/something-something", func(test *testing.T) {
 		expected := errors.New("invalid endpoint").Error()
-		_, err := api.HandleEndpoint("/api/user/995/something-something")
+		_, err := api.HandleEndpoint("/api/user/995/something-something", nil, nil)
 		result := err.Error()
 
 		if result != expected {
@@ -142,8 +144,56 @@ func TestAPIUserEndpoint(test *testing.T) {
 		}
 	})
 	test.Run("call12 /api/user/996/something-something", func(test *testing.T) {
+		// create a mock response writer & request
+		response := httptest.NewRecorder()
+		request, err := http.NewRequest("GET", "/api/html", nil)
+
+		_, err = api.HandleEndpoint("/api/user/996/something-something", response, request)
+
+		// EXPECTED
 		expected := errors.New("user id does not exist").Error()
-		_, err := api.HandleEndpoint("/api/user/996/something-something")
+
+		// RESULT
+		result := err.Error()
+
+		if result != expected {
+			test.Errorf("\t\tExpected %v, but got: %v \n", expected, result)
+		} else {
+			fmt.Printf("\t\tGot expected result: %v \n", expected)
+		}
+	})
+}
+func TestAPIHTMLEndpoint(test *testing.T) {
+	test.Run("call1 /api/html/", func(test *testing.T) {
+		// EXPECTED
+		expected := errors.New("not json response").Error()
+
+		// create a mock response writer & request
+		response := httptest.NewRecorder()
+		request, err := http.NewRequest("GET", "/api/html/", nil)
+
+		_, err = api.HandleEndpoint("/api/html/", response, request)
+
+		// RESULT
+		result := err.Error()
+
+		if result != expected {
+			test.Errorf("\t\tExpected %v, but got: %v \n", expected, result)
+		} else {
+			fmt.Printf("\t\tGot expected result: %v \n", expected)
+		}
+	})
+	test.Run("call8 /api/html", func(test *testing.T) {
+		// create a mock response writer & request
+		response := httptest.NewRecorder()
+		request, err := http.NewRequest("GET", "/api/html", nil)
+
+		_, err = api.HandleEndpoint("/api/html", response, request)
+
+		// EXPECTED
+		expected := errors.New("not json response").Error()
+
+		// RESULT
 		result := err.Error()
 
 		if result != expected {
