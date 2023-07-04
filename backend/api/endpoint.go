873292cfa821963cd8510a87291f2d4e77fc9aa5
Lars Morten Bek <lars@beksoft.dk>
2023-07-04 18:37:43 +0200

    endpoint.go: Modified * A couple of features below was added to give an example of how changed content-type can make it possible to use the API to give for example downloads. Even though i would recommend to to this from the main.go by adding a http.HandleFunc that would use /downloads/, i have added them into this example. The user of this example should free to choose their own architectural design (and remove these). The following have been added to the example: - made download endpoint (example) - made png endpoint (example) - made html endpoint (example)

--- backend/api/endpoint.go
+++ backend/api/endpoint.go
@@ -1,26 +1,36 @@
 package api
 
 import (
+	"api/download"
 	"api/example"
 	"api/functions"
+	"api/html"
 	"api/links"
 	"api/native"
 	"api/places"
+	"api/png"
 	"api/user"
 	"api/users"
 	"errors"
+	"net/http"
 )
 
 // path: /api/*
 
-func HandleEndpoint(endpoint string) (any, error) {
+func HandleEndpoint(endpoint string, response http.ResponseWriter, request *http.Request) (any, error) {
 	// (Example) start with: /api/user/995/name
 	reducedEndpoint := functions.ReducedFirstLayerEndpoint(endpoint) // (Example) we get: user/995/name/something
 	currentEndpoint := functions.CurrentLayer(reducedEndpoint)       // (Example) we get: user
 
 	switch currentEndpoint {
+	case "download":
+		return download.Start(response, request)
 	case "native":
 		return native.HandleEndpoint(reducedEndpoint)
+	case "png":
+		return png.Get(response, request)
+	case "html":
+		return html.Get(response, request)
 	case "example":
 		return example.List(true)
 	case "links":
