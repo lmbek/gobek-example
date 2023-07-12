14fb3ee6df1314f4b2cddbc6323176d890ea9d63
Lars Morten Bek <lars@beksoft.dk>
2023-07-12 09:58:26 +0200

    session-cookie.go: Refactored * Changed namings for easier reading, more clean code

--- backend/api/endpoints/auth/session/session-cookie.go
+++ backend/api/endpoints/auth/session/session-cookie.go
@@ -19,15 +19,15 @@ func generateRandomBytes(n int) ([]byte, error) {
 
 // generateSessionToken generates a random session token using base64 encoding.
 func generateSessionToken() (string, error) {
-	b, err := generateRandomBytes(32) // Adjust token length as per your requirements
+	bytes, err := generateRandomBytes(32) // Adjust token length as per your requirements
 	if err != nil {
 		return "", err
 	}
-	return base64.URLEncoding.EncodeToString(b), nil
+	return base64.URLEncoding.EncodeToString(bytes), nil
 }
 
 // createSessionCookie creates a new session cookie.
-func createSessionCookie(w http.ResponseWriter, sessionToken string) {
+func createSessionCookie(response http.ResponseWriter, sessionToken string) {
 	var cookie = http.Cookie{
 		Name:     "my_little_session_cookie",
 		Value:    sessionToken, // Set doing runtime
@@ -37,7 +37,7 @@ func createSessionCookie(w http.ResponseWriter, sessionToken string) {
 		Secure:   true,
 		SameSite: http.SameSiteStrictMode,
 	}
-	http.SetCookie(w, &cookie)
+	http.SetCookie(response, &cookie)
 }
 
 // deleteSessionCookie deletes the session cookie by setting its expiration to the past.
@@ -55,15 +55,15 @@ func deleteSessionCookie(w http.ResponseWriter) {
 }
 
 // Active retrieves the session token from the request cookie.
-func Active(w http.ResponseWriter, r *http.Request) (string, error) {
-	_, err := r.Cookie("my_little_session_cookie")
+func Active(response http.ResponseWriter, request *http.Request) (string, error) {
+	_, err := request.Cookie("my_little_session_cookie")
 	if err != nil {
 		return "inactive", err
 	}
 	return "active", nil
 }
 
-func Create(w http.ResponseWriter, r *http.Request, userID string) (any, error) {
+func Create(response http.ResponseWriter, r *http.Request, userID string) (any, error) {
 	// Generate session token
 	sessionToken, err := generateSessionToken()
 	if err != nil {
@@ -71,7 +71,7 @@ func Create(w http.ResponseWriter, r *http.Request, userID string) (any, error) 
 	}
 
 	// Create session cookie
-	createSessionCookie(w, sessionToken)
+	createSessionCookie(response, sessionToken)
 	return "Started session", nil
 }
 
