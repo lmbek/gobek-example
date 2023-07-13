aec7c1dc72794bb011476c189b981b1dedffb9ff
Lars Morten Bek <lars@beksoft.dk>
2023-07-13 03:45:39 +0200

    links.go: Modified by adding comments

--- backend/api/endpoints/links/links.go
+++ backend/api/endpoints/links/links.go
@@ -5,10 +5,12 @@ import (
 )
 
 func Get() (any, error) {
+	// if we have data, send data
 	if true {
 		myData := []string{"https://google.dk", "https://facebook.com", "https://linkedin.com"}
 		return myData, nil
 	}
 
+	// else send the error (modify current error)
 	return nil, errors.New("database could not connect")
 }
